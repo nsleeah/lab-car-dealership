220bff3673c6b9d8153cb285508e3f494b892e5b
Leahnicoles <lnsimonn@hotmail.com>
2023-03-01 19:24:40 +0000

    add constructor to car.js and constructor to dealership.js

---
+++ dealership.js
@@ -0,0 +1,7 @@
+const Dealership = function(name, maxNumberOfCars, carsInStock){
+    this.name = name;
+    this.maxNumberOfCars = maxNumberOfCars;
+    this.carsInStock = carsInStock;
+};
+
+module.exports = {Dealership};
